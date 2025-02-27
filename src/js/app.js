f1a516940deb628b67bf0db32bb4b3b1d2f13d32
AnneRom <coolann000.com@gmail.com>
2025-02-27 17:20:49 +0200

    Lesson_24-02
    адаптація хедера.

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,13 @@ console.log("Hello!");
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const menuToggle = document.querySelector('.sideMenu__icon');
+const sideMenu = document.querySelector('.header__nav');
+
+document.addEventListener('DOMContentLoaded', () => {
+    menuToggle.addEventListener('click', () => {
+        sideMenu.classList.toggle('active_sideMenu');
+        menuToggle.classList.toggle('open');
+    })
+});

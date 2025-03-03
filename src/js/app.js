6b2748cdc9eaa26f491badab8d317aad99806405
AnneRom <coolann000.com@gmail.com>
2025-03-03 18:18:01 +0200

    Lesson_03-05
    Доробили хедер.

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,8 @@ flsFunctions.isWebp();
 
 const menuToggle = document.querySelector('.sideMenu__icon');
 const sideMenu = document.querySelector('.header__nav');
+let lastScrollTop = 0;
+const header = document.querySelector('.header');
 
 document.addEventListener('DOMContentLoaded', () => {
     menuToggle.addEventListener('click', () => {
@@ -14,8 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     window.addEventListener('resize', () => {
         if (window.innerWidth > 850) {
+            sideMenu.classList.add('no-transition'); 
             sideMenu.classList.remove('active_sideMenu');
             menuToggle.classList.remove('open');
+        } 
+        else {
+            sideMenu.classList.remove('no-transition');
         }
+    });
+    window.addEventListener ('scroll', () => {
+        let scrollTop = window.scrollY || document.documentElement.scrollTop;
+        
+        if ((scrollTop > lastScrollTop) && (scrollTop > header.offsetHeight)) {
+            header.classList.add('hidden');
+        } else {
+            header.classList.remove('hidden');
+        } 
+    
+        lastScrollTop = scrollTop;
     });
 });

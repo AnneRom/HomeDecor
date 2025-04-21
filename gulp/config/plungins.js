b60610ec210fd54ac8b8832c3ec781b2bee6a4a6
AnneRom <coolann000.com@gmail.com>
2025-04-21 18:33:01 +0300

    Lesson_21-04
    доробили проєкт,
    почали робити режим продакшена

--- gulp/config/plungins.js
+++ gulp/config/plungins.js
@@ -3,11 +3,13 @@ import browsersync from "browser-sync";
 import notify from "gulp-notify";
 import plumber from "gulp-plumber";
 import newer from "gulp-newer";
+import ifPlugin from "gulp-if";
 
 export const plugins = {
     replace: replace,
     notify: notify,
     browsersync: browsersync,
     plumber: plumber,
-    newer: newer
+    newer: newer,
+    if: ifPlugin
 }
